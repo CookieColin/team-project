5761b36382fc7ce49ab4482f9f304e7cb3bbc25b
Nameer Qadir <nameer.qadir@mail.utoronto.ca>
2025-11-08 22:20:35 -0500

    Added preliminary recipe class

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Recipe {
+    private String name;
+    private ArrayList<String> ingredients ;
+    private ArrayList<String> steps;
+    private int prepTime;
+    private int cookTime;
+    private int servings;
+    private String dietaryRestrictions;
+
+    public Recipe(String name, ArrayList<String> ingredients, ArrayList<String> steps, int prepTime, int cookTime, int servings, String dietaryRestrictions) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.prepTime = prepTime;
+        this.cookTime = cookTime;
+        this.servings = servings;
+        this.dietaryRestrictions = dietaryRestrictions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public ArrayList<String> getSteps() {
+        return steps;
+    }
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getDietaryRestrictions() {
+        return dietaryRestrictions;
+    }
+
 }
